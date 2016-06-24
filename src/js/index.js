0228dda535add41019af8e5eb4af90cb9654701a
artem <artem@smarthead.ru>
2016-06-24 19:01:35 +0300

    'init'

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@
 //---------------------------------------
   function randomArr(){
     var length = 3;
-    var arr = [8,0,6];
+    var arr = [8,6,6];
 
     //for (var i = 0; i < length; i++){
     //  var n = Math.random() * 10;
@@ -19,7 +19,7 @@
   function createList(arr, list){
     list.empty();
     for (var i = 0; i < arr.length; i++){
-      list.append('<li data-num="' + arr[i] +'"><div>' + arr[i] + '</div></li>');
+      list.append('<li data-num="' + arr[i] + i +'"><div>' + arr[i] + '</div></li>');
     }
   }
 //-----------------------------------------
@@ -39,30 +39,36 @@
   function sortList(arr){
     var tmp;
     var tmpY;
-
+    var tmpId;
+    var tmpIdNext;
     for (var i = arr.length - 1, time = 0; i > 0; i--) {
       for (var j = 0; j < i; j++) {
-        setTimeout( showSort(j) ,  5000 * (++time) );
+        setTimeout( showSort(j) ,  1000 * (++time) );
 
         function showSort(j){
-
           return function(){
             if (arr[j] > arr[j+1]) {
               tmp = arr[j];
-              //$('.unsorted-list li').eq(j).attr('data-num', arr[j+1]);
-              tmpY = parseInt( $('.unsorted-list li').eq(j).find('div').css('transform').split(',')[5] );
+              tmpY = parseInt( $('.unsorted-list li[data-num="' + arr[j] + j +'"]').find('div').css('transform').split(',')[5] );
+              tmpId = $('.unsorted-list li[data-num="' + arr[j] + j +'"]').data('num');
+              tmpIdNext = $('.unsorted-list li[data-num="' + arr[j+1] + ( j + 1 ) +'"]').data('num');
 
+
+
+
+              $('.unsorted-list li[data-num="' + arr[j] + j +'"]').find('div').css('transform', 'translate(0,' + parseInt( $('.unsorted-list li[data-num="' + arr[j+1] + (j+1) +'"]').find('div').css('transform').split(',')[5])  + 'px)');
               arr[j] = arr[j+1];
-              $('.unsorted-list li').eq(j).find('div').css('transform', 'translate(0,' + parseInt( $('.unsorted-list li').eq(j+1).find('div').css('transform').split(',')[5])  + 'px)');
 
+              $('.unsorted-list li[data-num="' + arr[j+1] + (j+1) +'"]').find('div').css('transform', 'translate(0,' + tmpY + 'px)');
               arr[j+1] = tmp;
-              $('.unsorted-list li').eq(j+1).find('div').css('transform', 'translate(0,' + tmpY + 'px)');
-              //$('.unsorted-list li').eq(j+1).attr('data-num', tmp);
-              console.log(arr);
+
+              $ ('.unsorted-list li[data-num="' + tmpId +'"]').attr('data-num',  tmpIdNext);
+              $('.unsorted-list li[data-num="' + tmpIdNext +'"]').attr('data-num', tmpId );
+              console.log(tmpId,tmpIdNext);
+
             }
           }
         }
-
       }
     }
 
@@ -72,6 +78,5 @@
 //---------------------------------------
   showList(btnCreateList);
   showSortList(btnSortList)
-
 
 }(jQuery));
