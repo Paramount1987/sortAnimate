01a6134567c83d839f5fbad1bcf026d238cdbfe6
artem <artem@smarthead.ru>
2016-06-27 15:29:42 +0300

    add info control

--- src/js/index.js
+++ src/js/index.js
@@ -1,100 +1,113 @@
 ;(function($){
 
-  var list = '.unsorted-list';
-  var btnCreateList = '.btn-create-list';
-  var btnSortList = '.btn-sort-list';
-  var arrRandom = [];
-  var textSort = '.text-sort';
-//---------------------------------------
-  function randomArr(){
-    var length = 9;
-    var arr = [];
+   var sortAnimate = {
 
-    for (var i = 0; i < length; i++){
-     var n = Math.random() * 10;
-     arr.push( Math.round(n) );
-    }
-    return arr;
-  }
-//-----------------------------------------
-  function createList(arr, list){
-    $(list).empty();
-    for (var i = 0; i < arr.length; i++){
-      $(list).append('<li data-num="' + arr[i] +'">' + arr[i] + '</li>');
-    }
-  }
-//-----------------------------------------
-  function showList(btn){
-    $('body').on('click', btnCreateList, function(){
-      arrRandom = randomArr();
-      createList(arrRandom, list);
-    });
-  }
-//----------------------------------------
-  function showSortList(btnSortList){
-    $('body').on('click', btnSortList, function(){
-      sortList(arrRandom, list);
-    });
-  }
-//----------------------------------------
-  function sortList(arr){
-    var tmp;
-    var tmpY;
-    startSorting();
+      list:          '.unsorted-list',
+      btnCreateList: '.btn-create-list',
+      btnSortList:   '.btn-sort-list',
+      arrRandom:     [],
+      textSort:      '.text-sort',
+      sortInfo:      '.sort-info',
 
-    for (var i = arr.length - 1, time = 1; i > 0; i--) {
-      for (var j = 0; j < i; j++) {
+       init: function(){
+          this.showListClick( this.btnCreateList, this.list );
+          this.showSortListClick( this.btnSortList, this.list );
+       },
+       //----------------------------generate random arr
+       randomArr: function(length){
+          var arr = [];
 
-          setTimeout( showSort(j, i) ,  400 * (++time) );
-            
-          function showSort(j, i){
-            return function(){
-
-              $(list + ' li').eq(j).addClass('active');
-
-              if (arr[j] > arr[j+1]) {
-
-                  tmp = arr[j];
-                  tmpY = parseInt( $(list + ' li').eq(j).css('transform').split(',')[5] );
-
-                  $(list + ' li').eq(j).css('transform', 'translate(0,' + parseInt( $(list + ' li').eq(j+1).css('transform').split(',')[5])  + 'px)');
-                  arr[j] = arr[j+1];
-
-                  $(list + ' li').eq(j+1).css('transform', 'translate(0,' + tmpY + 'px)');
-                  arr[j+1] = tmp;
-
-                  setTimeout(callbackInsert(j), 300);
-
-                  function callbackInsert(j){
-                    return function(){
-                      $(list + ' li').eq(j).insertAfter( $(list + ' li').eq(j+1) );
-                    }
-                        
-                  } 
-              }
-
-              setTimeout(function(){ $(list + ' li.active').removeClass('active'); }, 300);
-
-              if ( i == 1) {endSorting();}
-            }
+          for (var i = 0; i < length; i++){
+             arr.push( Math.round( Math.random() * 10 ));
           }
-      }
-    }
-  }
-//--------------------------------------
-function startSorting(){
-  $(textSort).text('list sorting...');
-  $(btnCreateList).prop('disabled', true); 
-  $(btnSortList).prop('disabled', true); 
-}
+          return arr;
+       },
+       //------------------------------create list from random arr
+       createList: function(arr, list){
+          $(list).empty();
 
-function endSorting(){
-  $(textSort).text('list sorting ended');
-  $(btnCreateList).prop('disabled', false); 
-  $(btnSortList).prop('disabled', false); 
-}
-//---------------------------------------
-  showList(btnCreateList);
-  showSortList(btnSortList)
+          for (var i = 0; i < arr.length; i++){
+             $(list).append('<li data-num="' + arr[i] +'">' + arr[i] + '</li>');
+          }
+       },
+       //------------------------------sort list from random arr
+       sortList: function(arr, list){
+          var tmp;
+          var tmpY;
+          var that = this;
+          that.startSorting();
 
+          for (var i = arr.length - 1, time = 1; i > 0; i--) {
+             for (var j = 0; j < i; j++) { //--обход массива каждым элемнетом
+
+                setTimeout( showSort(j, i) ,  500 * (++time) ); //таймаут для каждого обхода - для визуализации обхода
+
+                function showSort(j, i){
+                   return function(){
+
+                      $(list + ' li').eq(j).addClass('active');
+
+                      if (arr[j] > arr[j+1]) {
+
+                         tmp = arr[j];
+                         tmpY = parseInt( $(list + ' li').eq(j).css('transform').split(',')[5] );
+
+                         $(list + ' li').eq(j).css('transform', 'translate(0,' + parseInt( $(list + ' li').eq(j+1).css('transform').split(',')[5])  + 'px)');
+                         arr[j] = arr[j+1];
+
+                         $(list + ' li').eq(j+1).css('transform', 'translate(0,' + tmpY + 'px)');
+                         arr[j+1] = tmp;
+
+                         setTimeout(callbackInsert(j), 300); //изменение порядка li в dom
+
+                         function callbackInsert(j){
+                            return function(){
+                              $(list + ' li').eq(j).insertAfter( $(list + ' li').eq(j+1) );
+                            }
+                         }
+                      }
+
+                      setTimeout(function(){ $(list + ' li.active').removeClass('active'); }, 400);
+
+                      if ( i == 1) {that.endSorting();}
+                   }
+                }
+             }
+          }
+       },
+       //--------------------------------start sort status
+       startSorting: function(){
+          $(this.textSort).text('list sorting...');
+          $(this.btnCreateList).prop('disabled', true);
+          $(this.btnSortList).prop('disabled', true);
+          $(this.sortInfo).css('display', 'none');
+          $(this.sortInfo).find('div').eq(0).html( 'Before sort: ' + this.arrRandom);
+       },
+       //---------------------------------end sort status
+       endSorting: function(){
+          $(this.textSort).text('list sorting ended');
+          $(this.btnCreateList).prop('disabled', false);
+          $(this.btnSortList).prop('disabled', false);
+          $(this.sortInfo).css('display', 'block');
+          $(this.sortInfo).find('div').eq(1).html('After sort:&nbsp;&nbsp;&nbsp;&nbsp;' + this.arrRandom);
+       },
+       //-------------------------------show list from random arr
+       showListClick: function(btn, list){
+          var that = this;
+          $('body').on('click', btn, function(){
+             that.arrRandom = that.randomArr(9);
+             that.createList(that.arrRandom, list);
+             $(that.btnSortList).prop('disabled', false);
+         });
+       },
+       //---------------------------------show sort list
+       showSortListClick: function(btn, list){
+          var that = this;
+          $('body').on('click', btn, function(){
+             that.sortList(that.arrRandom, list);
+          });
+       }
+   };
+
+  sortAnimate.init();
 }(jQuery));
